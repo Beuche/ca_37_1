6df0a7c7dc9993e29c0e80cd77a07ff12e56a69a
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-24 15:13:11 +0100

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 zeit=0
 while True:
     zeit=Math.round(input.running_time()/1000)
-    if (zeit % 2) == 1 :
-        basic.set_led_color(#500000)
+    if zeit % 2 :
+        basic.set_led_color(0x500000)
     else :
-        basic.set_led_color(#005000)
-    
-    
+        basic.set_led_color(0x005000)
+       
+   
--- main.ts
+++ main.ts
@@ -2,10 +2,9 @@ let zeit = 0
 while (true) {
     zeit = Math.round(input.runningTime() / 1000)
     if (zeit % 2) {
-        basic.setLedColor
+        basic.setLedColor(0x500000)
     } else {
-        // ff0000))
-        
+        basic.setLedColor(0x005000)
     }
     
 }
